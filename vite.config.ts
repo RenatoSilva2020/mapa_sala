f6fbac4014ab5859e2595f5511b833d7b496560c
renatosilva2020 <professorrsilva2020@gmail.com>
2026-03-22 18:23:25 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/mapa_salamp/',
+    base: '/mapa_sala/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
